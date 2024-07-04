cdd91cda57ab02175bd8827c8546e89de16a9d92
Arnav Samal <samalarnav@gmail.com>
2024-07-04 16:09:22 +0530

    adding constructor vector

--- stl/containers-iterators/vectors.cpp
+++ stl/containers-iterators/vectors.cpp
@@ -56,9 +56,22 @@ int main()
 
     v.empty();
 
+    // constructors used in the same order as described above:
+    vector<int> first;                                // empty vector of ints
+    vector<int> second (4,100);                       // four ints with value 100
+    vector<int> third (second.begin(),second.end());  // iterating through second
+    vector<int> fourth (third);                       // a copy of third
+
+    // the iterator constructor can also be used to construct from arrays:
+    int myints[] = {16,2,77,29};
+    vector<int> fifth (myints, myints + sizeof(myints) / sizeof(int) );
+
+    cout << "The contents of fifth are:";
+    for (vector<int>::iterator it = fifth.begin(); it != fifth.end(); ++it)
+        cout << ' ' << *it;
+    cout << '\n';
 
 
     return 0;
-
     // uses linked list
 }
